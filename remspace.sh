06320b7a6c0e7ed4ae2349c756d3c7d4f3caf37c
kevna <aam13@aber.ac.uk>
2013-07-30 15:35:58 +0100

    initial push

---
+++ remspace.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#script by Aaron Moore (Kevna), all rights reserved
+#iteratively cleans up filenames by replacing spaces
+#or underscores or sequences of both of them and '-'
+#with '-' characters
+
+
+ls | while read -r FILE; do
+	mv -v "$FILE" `echo $FILE | tr ' ' '_' | tr -d '[{}(),\!]' | tr -d "\'" | tr '[A-Z]' '[a-z]' | sed 's/[-_]\+/_/g' | sed '/_$/d'`
+done
