c7611c57b6c90e352324bc419114801bdd8ca980
kevna <aam13@aber.ac.uk>
2013-08-08 15:54:33 +0100

    aligned whitespace

--- backup.sh
+++ backup.sh
@@ -15,18 +15,27 @@ if [[ "$HOSTNAME" != "central.aber.ac.uk" ]]; then
 fi
 
 if [ ! -e ~/backup.txt ]; then exit 0; fi
-if [ -e ${tarballFile}.tar.gz ]; then mv ${tarballFile}.tar.gz ${tarballFile}.old; fi;
-tar cvf ${tarballFile}.tar scripts > backup.log
-if [ $? == 0 ]; then echo "tarball created (scripts).";
-else echo "tarball creation failed."; exit 1; fi;
-for files in $(cat ~/scripts/backup.txt | grep -v \#); do
-	if [ ! $(echo $files|cut -c1-1) == '#'  ]; then if [ -e $files ]; then
-		tar rvf ${tarballFile}.tar $files >> backup.log
-		if [ $? == 0 ]; then echo "$files added to tarball.";
-		else echo "failed to add $files to tarball."; fi;
-	else echo "$files is not an existing directory or file."; fi; fi;
-done
+if [ -e ${tarballFile}.tar.gz ]; then
+	mv ${tarballFile}.tar.gz ${tarballFile}.old; fi;
+	tar cvf ${tarballFile}.tar scripts > backup.log
+	if [ $? == 0 ]; then echo "tarball created (scripts).";
+		else echo "tarball creation failed."; exit 1; fi;
+		for files in $(cat ~/scripts/backup.txt | grep -v \#); do
+			if [ -e $files ]; then
+				tar rvf ${tarballFile}.tar $files >> backup.log
+				if [ $? == 0 ]; then echo "$files added to tarball.";
+					else echo "failed to add $files to tarball.";
+				fi;
+			else
+				echo "$files is not an existing directory or file.";
+			fi;
+		done
 echo "compressing tarball."
 gzip ${tarballFile}.tar
-if [ $? == 0 ]; then echo "done."; else echo "compression failed."; exit 1; fi;
+if [ $? == 0 ]; then
+	echo "done.";
+else
+	echo "compression failed.";
+	exit 1;
+fi;
 
