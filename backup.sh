99f250d3f6dcd11dae97678ed130e027de4ac33a
kevna <aam13@aber.ac.uk>
2013-10-28 09:12:39 +0000

    externalised logfile filepath and moved to regex servername handling

--- backup.sh
+++ backup.sh
@@ -1,34 +1,35 @@
 #!/bin/sh
-#script by Aaron Moore (Kevna), all rights reserved
+#script by Aaron Moore (kevna), all rights reserved
 #tarball ~/scripts and all files named in backup.txt
 #verbose output in backup.log
 
-tarballFile="mtarball"
+tarballFile=~/mtarball
+logFile=~/backup.log
 
 cd
 echo $HOSTNAME
-if [[ "$HOSTNAME" != "central.aber.ac.uk" ]]; then
+if [[ "$HOSTNAME" =~ "^.*\.aber\.ac\.uk" ]]; then
 	echo "calling remote backup."
 	ssh central 'backup.sh'
 	echo "copying backup file."
-	scp central:mtarball.tar.gz ~/
+	scp central:mtarball.tgz ~/
 fi
 
-if [[ ! -e ~/backup.txt ]]; then exit 0; fi
-if [[ -e ${tarballFile}.tar.gz ]]; then
-	mv ${tarballFile}.tar.gz ${tarballFile}.old
+if [ ! -e ~/backup.txt ]; then exit 0; fi
+if [ -e ${tarballFile}.tgz ]; then
+	mv ${tarballFile}.tgz ${tarballFile}.old
 fi
-tar cvf ${tarballFile}.tar scripts > backup.log
-if [[ $? == 0 ]]; then
+tar cvf $tarballFile scripts > $logFile
+if [ $? == 0 ]; then
 	echo "tarball created (scripts)."
 else
 	echo "tarball creation failed." >&2
-	exit 1
+	exit 2
 fi
 	for files in $(cat ~/backup.txt | grep -v \#); do
-		if [[ -e $files ]]; then
-			tar rvf ${tarballFile}.tar $files >> backup.log
-			if [[ $? == 0 ]]; then
+		if [ -e $files ]; then
+			tar rvf $tarballFile $files >> $logFile
+			if [ $? == 0 ]; then
 				echo "$files added to tarball."
 			else
 				echo "failed to add $files to tarball." >&2
@@ -38,11 +39,11 @@ fi
 		fi
 	done
 echo "compressing tarball."
-gzip ${tarballFile}.tar
-if [[ $? == 0 ]]; then
+gzip $tarballFile -S .tgz -v >> $logFile 2>&1
+if [ $? == 0 ]; then
 	echo "done."
 else
 	echo "compression failed." >&2
-	exit 1
+	exit 3
 fi
 
